c341885ba9b2c1cf247a7dfe52311b84b0481be1
charliembrodie <cbrodi01@cspc054.dcs.bbk.ac.uk>
2012-10-04 13:26:49 +0100

    Finished the numberPyramids exercise, full functionaliy

---
+++ numberPyramids.groovy
@@ -0,0 +1,12 @@
+
+print "Please enter the size of your number pyramid: "
+String str = System.console().readLine()
+int i = Integer.parseInt(str)
+
+for(int a = 1; a <=i; a++){
+ int b = a
+ println " "
+ for(int c =1; c <=a; c++){ 
+ print b
+ }
+}
